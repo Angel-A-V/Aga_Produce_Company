f6d63e68f954d428945fadf021f3098aaf6a6cdd
Angel Vargas <angelavargas0831@gmail.com>
2026-05-25 18:53:00 -0700

    Another switc, vite

---
+++ aga-produce/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar'
+
+
+function App() {
+    return(
+        <>
+            <Navbar />
+            { /* rest of the site */}
+        </>
+    )
+}
+
+export default App
---
+++ aga-produce/src/main.jsx
@@ -0,0 +1,11 @@
+// Find the div in html, turn on Reactm and load the website into it.
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
